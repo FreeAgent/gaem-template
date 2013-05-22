fa724908012b82c114a212b7245d145443867cc8
Kevin Noonan <kevin@chimera>
2013-05-22 14:37:49 +0100

    advanced version to 0.2.1; added project description and URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject gaem/lein-template "0.2.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject gaem/lein-template "0.2.1-SNAPSHOT"
+  :description "Leiningen template for new gaem-managed appengine-magic project."
+  :url "https://github.com/greynolds/gaem-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
